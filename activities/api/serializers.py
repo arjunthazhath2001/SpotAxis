ce2bd2a3f7d4210da2dfd73d39c09e6b7108a2cf
Arjun T A <arjunta32@gmail.com>
2025-05-21 20:23:16 +0530

    serializers completed

---
+++ activities/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from activities.models import * 
+
+class MessageChunkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= MessageChunk
+        fields='__all__'
+        read_only_fields=('last_updated')
+    
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Activity
+        fields='__all__'
+        read_only_fields=('timestamp','last_updated')
+
+        
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Notification
+        fields='__all__'
+        read_only_fields=('timestamp','last_updated','seen','actor_count')
+
+
